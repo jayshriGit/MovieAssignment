abdf2a3fa9a2ae049dd0e7d00e689a7fd0fcb96c
jayhsri <patiljayshri2519@gmail.com>
2020-10-07 22:59:38 +0530

    conditional check added to check plots are loaded or not

--- src/components/show-movies.js
+++ src/components/show-movies.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./show-movies.css";
 import { StarRating } from "./star-rating";
 
-
 export class ShowMovies extends React.Component {
   constructor(props) {
     super(props);
@@ -25,13 +24,14 @@ export class ShowMovies extends React.Component {
       })
       .catch((error) => this.setState({ error, isLoading: false }));
   }
-  
+
   processData = (prom) => {
     const details = this.state.movieDetails;
     prom.then((data) => {
-      details.push(data);
+      if (data.id !== null) details.push(data);
     });
-    this.setState({ ...this.state, movieDetails: details });
+
+    this.setState({ ...this.state, movieDetails: details, isLoading: false });
   };
 
   componentDidMount() {
@@ -40,6 +40,7 @@ export class ShowMovies extends React.Component {
 
   render() {
     const { movies, isMobileView } = this.props;
+    const { isLoading, movieDetails } = this.state;
     const ratingColor = [
       "white",
       "yellow",
@@ -52,52 +53,64 @@ export class ShowMovies extends React.Component {
       "red",
       "purple",
     ];
-    // const { year,image,plot } = this.state.movieDetails;
-    const plot =
-      " Fight Club is a 1999 American film directed by David Fincher and starring Brad Pitt, Edward Norton, and Helena Bonham Carter. It is based on the 1996 novel of the same name by Chuck Palahniuk. Norton plays the unnamed narrator, who is discontented with his white-collar job.";
-    return (
-      <div>
-        {movies.map((movie, index) => {
-          const {
-            title,
-            image,
-            fullTitle,
-            imDbRatingCount,
-            imDbRating,
-          } = movie;
-          const ratingCount = imDbRatingCount.substr(
-            0,
-            imDbRatingCount.length - 3
-          );
-          const ReleaseYear = fullTitle.substr(fullTitle.indexOf("(") + 1, 4);
-          return (
-            <div key={index} className="movie-container">
-              <div className="movie-title">
-                <h3>{title}</h3>
-                <p>{plot}</p>
-                <p>Year- {ReleaseYear}</p>
-                {!isMobileView ? (
-                  <p style={{ color: ratingColor[Math.floor(imDbRating)] }}>
-                    Ratings-{imDbRating}
-                  </p>
-                ) : (
-                  <StarRating count={Math.floor(imDbRating)} />
-                )}
-              </div>
 
-              <div>
-                <img
-                  className="image-container"
-                  src={image}
-                  alt={fullTitle}
-                  height="200"
-                  // width={isMobileView ? "200" : "300"}
-                />
-                <p>Rating Count-{ratingCount}K</p>
-              </div>
-            </div>
-          );
-        })}
+    return (
+      <div style={{ textAlign: "center" }}>
+        {!isLoading && movieDetails.length > 0 ? (
+          <div>
+            {movies.map((movie, index) => {
+              const {
+                title,
+                image,
+                fullTitle,
+                imDbRatingCount,
+                imDbRating,
+              } = movie;
+              const ratingCount = imDbRatingCount.substr(
+                0,
+                imDbRatingCount.length - 3
+              );
+              const ReleaseYear = fullTitle.substr(
+                fullTitle.indexOf("(") + 1,
+                4
+              );
+
+              const plot = this.state.movieDetails.find(
+                (item) => item.id === movie.id
+              ).plot;
+
+              return (
+                <div key={index} className="movie-container">
+                  <div className="movie-title">
+                    <h3>{title}</h3>
+                    <p>{plot}</p>
+                    <p>Year- {ReleaseYear}</p>
+                    {!isMobileView ? (
+                      <p style={{ color: ratingColor[Math.floor(imDbRating)] }}>
+                        Ratings-{imDbRating}
+                      </p>
+                    ) : (
+                      <StarRating count={Math.floor(imDbRating)} />
+                    )}
+                  </div>
+
+                  <div>
+                    <img
+                      className="image-container"
+                      src={image}
+                      alt={fullTitle}
+                      height="200"
+                      // width={isMobileView ? "200" : "300"}
+                    />
+                    <p>Rating Count-{ratingCount}K</p>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <h4>loading ...</h4>
+        )}
       </div>
     );
   }
